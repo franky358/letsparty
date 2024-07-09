ac032a0c6cfdfa571fe47c1373086de1ff62b2d3
Frank Andrade <fr4nky@durazno.io>
2024-07-09 09:24:38 -0600

    added clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "../index.scss";
+import Script from "next/script";
 import NoSsr from "../utils/NoSsr";
 const BaseCSS = ({ css = "*{box-sizing:border-box}body{margin:0}" }) => (
   <style
@@ -53,6 +54,15 @@ export default function RootLayout({ children }) {
       </head>
       <body className="try">
         <NoSsr>{children}</NoSsr>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
