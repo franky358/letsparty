94771111f858c3a89bf842a6745e47a6eeac3553
Frank Andrade <fr4nky@durazno.io>
2024-07-08 21:59:46 -0600

    updated title and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,22 +8,47 @@ const BaseCSS = ({ css = "*{box-sizing:border-box}body{margin:0}" }) => (
   />
 );
 
-
 export default function RootLayout({ children }) {
   return (
     <html>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="generator" content="mdx-docs" />
-        <title>Unice</title>
-        <link rel="icon" href="/assets/images/logo/favicon.png" type="image/x-icon" />
-        <link rel="shortcut icon" href="/assets/images/logo/favicon.png" type="image/x-icon" />
-        <link href="https://fonts.googleapis.com/css?family=Capriola&amp;subset=latin-ext" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Alex+Brush&amp;subset=latin-ext" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700,800,900&display=swap" rel="stylesheet"></link>
-        <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800" rel="stylesheet" />
+        <title>Let's Party</title>
+        <link
+          rel="icon"
+          href="/assets/images/logo/favicon.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="shortcut icon"
+          href="/assets/images/logo/favicon.png"
+          type="image/x-icon"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Capriola&amp;subset=latin-ext"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Satisfy"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css?family=Alex+Brush&amp;subset=latin-ext"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css?family=Raleway:400,600,700,800,900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800"
+          rel="stylesheet"
+        />
         <BaseCSS />
       </head>
       <body className="try">
