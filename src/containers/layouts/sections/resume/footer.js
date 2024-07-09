ca6a8ab1dc113a2b70e4aa66eedf813a412eb2a4
Frank Andrade <fr4nky@durazno.io>
2024-07-08 23:47:08 -0600

    target _blank

--- src/containers/layouts/sections/resume/footer.js
+++ src/containers/layouts/sections/resume/footer.js
@@ -12,6 +12,7 @@ const Footer = () => (
                   <a
                     className="copyright-text "
                     href="https://www.instagram.com/letspartyevents_mx/"
+                    target="_blank"
                   >
                     <i aria-hidden="true" className="fa fa-instagram"></i>
                   </a>
@@ -20,6 +21,7 @@ const Footer = () => (
                   <a
                     className="copyright-text"
                     href="https://www.facebook.com/letspartyeventsmx?mibextid=LQQJ4d&rdid=QtmdRJwo7WjFPLjW&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2Frc2EVFu8YM96MoXJ%2F%3Fmibextid%3DLQQJ4d"
+                    target="_blank"
                   >
                     <i aria-hidden="true" className="fa fa-facebook"></i>
                   </a>
